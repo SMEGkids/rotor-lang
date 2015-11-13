3c53394b5fe8d66fa5f03e6b4569d7e5956aa488
Hipe99 <Hipe99@users.noreply.github.com>
2015-11-14 06:46:42 +1100

    Wrote framework

--- rotor.py
+++ rotor.py
@@ -13,4 +13,20 @@
 #GNU General Public License for more details.
 
 #You should have received a copy of the GNU General Public License
-#along with Rotor.  If not, see <http://www.gnu.org/licenses/>.
+#along with Rotor. 
+#If not, see <http://www.gnu.org/licenses/>.
+wheels={}
+special={}
+program=input('>')
+currentwheel=0
+stack=[]
+for i in program:
+    if i=='>':
+        currentwheel=(currentwheel+1)%len(wheels)
+    elif i=='<':
+        currentwheel=(currentwheel-1) if currentwheel!=0 else len(wheels)
+    elif i in list(special.keys):
+        special[i](stack)
+    elif i in wheels[currentwheel]:
+        wheels[currentwheel][i](stack)
+        
